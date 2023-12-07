c8d63501a1a7183674bed2a00a084d42c28704ce
florianbsc <floflo_200068@hotmail.fr>
2023-12-07 14:16:49 +0100

    fin de la premiere video creation d'un MVC pour affichier des articles en fonction de leurs id, si un id n'exsite pas dans la db --> erreur 404

--- laravel/app/Http/Controllers/PostController.php
+++ laravel/app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('home', [
+        return view('products/home', [
             'articles' => Post::all()
         ]);
     }
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 // vue interface utilisateur
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 
 Route::get('/', [\App\Http\Controllers\PostController::class, 'index'] );
 Route::get('{post}',[\App\Http\Controllers\PostController::class, 'show'] );
+Route::get('/product', [\App\Http\Controllers\ProductController::class, 'home'])->name('product.home');
